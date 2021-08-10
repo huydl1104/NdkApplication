5d83fe0cab6be8d93d6e1929e83bda80fac71f04
余栋梁 <yudongliang@okay.cn>
2021-08-10 17:41:09 +0800

    增加 视频地址

--- app/src/main/java/com/example/ndk/MusicActivity.kt
+++ app/src/main/java/com/example/ndk/MusicActivity.kt
@@ -1,19 +1,23 @@
 package com.example.ndk
 
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ndk.player.MusicPlayer
+import java.io.File
 
 
 class MusicActivity : AppCompatActivity() {
+
+    var mMusicFile = File(Environment.getExternalStorageDirectory(), "video.mp4")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val player = MediaPlayer()
         val musicPlayer = MusicPlayer()
-        musicPlayer.play("asd")
+        musicPlayer.play(mMusicFile.absolutePath)
     }
 
 }
