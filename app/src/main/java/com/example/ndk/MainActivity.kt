4efec7c0384199befb4800e08806074eabae752c
余栋梁 <yudongliang@okay.cn>
2021-08-12 15:12:12 +0800

    升级 ffmpeg 配置信息

---
+++ app/src/main/java/com/example/ndk/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.ndk
+
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    }
+}
